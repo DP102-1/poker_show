408179097508a3827b46d431365f68629a50a03f
wong <wq156666@gmail.com>
2019-09-16 11:56:20 +0800

    //柴犬很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇 很兇

--- app/src/main/java/com/example/poker_show/MainActivity.java
+++ app/src/main/java/com/example/poker_show/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.poker_show;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageView image_poker;
     private SeekBar sb;
     String flower = "方塊";
-    int num = 1;
+    int num = 0;
+    List<Integer> poker_d = new ArrayList<>();
+    List<Integer> poker_s = new ArrayList<>();
+    List<Integer> poker_h = new ArrayList<>();
+    List<Integer> poker_c = new ArrayList<>();
 
 
     @Override
@@ -29,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         poker_text = findViewById(R.id.textView);
         number = findViewById(R.id.number);
         sb = findViewById(R.id.seekBar);
-        List<Integer> poker = new ArrayList<Integer>();
+        setPoker_d();
+        setPoker_s();
+        setPoker_h();
+        setPoker_c();
+
 
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -38,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                                           int progress,
                                           boolean fromUser) {
                 num =+ progress;
-                num++;
                 poker_show();
                 number.setText(num);
             }
@@ -71,23 +79,82 @@ public class MainActivity extends AppCompatActivity {
         poker_show();
     }
     public void poker_show(){
-        String setDrawable = "R.drawable.";
         switch (flower) {
                 case "方塊":
-                image_poker.setImageResource(Integer.parseInt(setDrawable + "d" + num));
+                image_poker.setImageResource(poker_d.get(num));
                 break;
                 case "黑桃":
-                image_poker.setImageResource(R.drawable.s1);
+                image_poker.setImageResource(poker_s.get(num));
                 break;
                 case "紅心":
-                image_poker.setImageResource(R.drawable.h1);
+                image_poker.setImageResource(poker_h.get(num));
                 break;
                 case "梅花":
-                image_poker.setImageResource(R.drawable.c1);
+                image_poker.setImageResource(poker_c.get(num));
                 break;
         }
 
         poker_text.setText(flower + num);
     }
 
+        public void setPoker_d(){
+        poker_d.add(R.drawable.d1);
+        poker_d.add(R.drawable.d2);
+        poker_d.add(R.drawable.d3);
+        poker_d.add(R.drawable.d4);
+        poker_d.add(R.drawable.d5);
+        poker_d.add(R.drawable.d6);
+        poker_d.add(R.drawable.d7);
+        poker_d.add(R.drawable.d8);
+        poker_d.add(R.drawable.d9);
+        poker_d.add(R.drawable.d10);
+        poker_d.add(R.drawable.d11);
+        poker_d.add(R.drawable.d12);
+        poker_d.add(R.drawable.d13);
+    }
+    public void setPoker_s(){
+        poker_d.add(R.drawable.s1);
+        poker_d.add(R.drawable.s2);
+        poker_d.add(R.drawable.s3);
+        poker_d.add(R.drawable.s4);
+        poker_d.add(R.drawable.s5);
+        poker_d.add(R.drawable.s6);
+        poker_d.add(R.drawable.s7);
+        poker_d.add(R.drawable.s8);
+        poker_d.add(R.drawable.s9);
+        poker_d.add(R.drawable.s10);
+        poker_d.add(R.drawable.s11);
+        poker_d.add(R.drawable.s12);
+        poker_d.add(R.drawable.s13);
+    }
+    public void setPoker_c(){
+        poker_d.add(R.drawable.c1);
+        poker_d.add(R.drawable.c2);
+        poker_d.add(R.drawable.c3);
+        poker_d.add(R.drawable.c4);
+        poker_d.add(R.drawable.c5);
+        poker_d.add(R.drawable.c6);
+        poker_d.add(R.drawable.c7);
+        poker_d.add(R.drawable.c8);
+        poker_d.add(R.drawable.c9);
+        poker_d.add(R.drawable.c10);
+        poker_d.add(R.drawable.c11);
+        poker_d.add(R.drawable.c12);
+        poker_d.add(R.drawable.c13);
+    }
+    public void setPoker_h(){
+        poker_d.add(R.drawable.h1);
+        poker_d.add(R.drawable.h2);
+        poker_d.add(R.drawable.h3);
+        poker_d.add(R.drawable.h4);
+        poker_d.add(R.drawable.h5);
+        poker_d.add(R.drawable.h6);
+        poker_d.add(R.drawable.h7);
+        poker_d.add(R.drawable.h8);
+        poker_d.add(R.drawable.h9);
+        poker_d.add(R.drawable.h10);
+        poker_d.add(R.drawable.h11);
+        poker_d.add(R.drawable.h12);
+        poker_d.add(R.drawable.h13);
+    }
 }
